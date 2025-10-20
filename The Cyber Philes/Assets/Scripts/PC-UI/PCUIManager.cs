d110d1682add6ef2784c332aafda6bdc073b9366
Anthony Rumore <anthony.rumore@ufl.edu>
2025-10-20 11:52:14 -0400

    Added TextView & FileFind to PCUI

--- The Cyber Philes/Assets/Scripts/PC-UI/PCUIManager.cs
+++ The Cyber Philes/Assets/Scripts/PC-UI/PCUIManager.cs
@@ -48,6 +48,16 @@ public class UIScreenManager : MonoBehaviour
 
     private void WireButtons(string screenName)
     {
+
+        if (!screenName.Equals("1010") && !screenName.Equals("1011"))
+        {
+            // Show dock on all screens not the login screen
+            root.Q<Button>("DockFileFindButton")?.RegisterCallback<ClickEvent>(_ => ShowScreen("1110"));
+            root.Q<Button>("DockWebBrowserButton")?.RegisterCallback<ClickEvent>(_ => ShowScreen("1130"));
+            root.Q<Button>("DockPasswordManButton")?.RegisterCallback<ClickEvent>(_ => ShowScreen("1150"));
+            root.Q<Button>("DockTextViewButton")?.RegisterCallback<ClickEvent>(_ => ShowScreen("1122"));
+        }
+
         // Add buttons for each screen
         switch (screenName)
         {
@@ -63,15 +73,17 @@ public class UIScreenManager : MonoBehaviour
                         ShowScreen("1011"); // Incorrect login
                 };
                 break;
-    
-
-            case "1100":
-                root.Q<Button>("DockFileFindButton")?.RegisterCallback<ClickEvent>(_ => ShowScreen("1110"));
-                root.Q<Button>("DockWebBrowserButton")?.RegisterCallback<ClickEvent>(_ => ShowScreen("Browser"));
-                break;
 
             case "1110":
                 root.Q<Button>("CloseButton")?.RegisterCallback<ClickEvent>(_ => ShowScreen("1100"));
+                root.Q<Button>("2faButton")?.RegisterCallback<ClickEvent>(_ => ShowScreen("1121"));
+                root.Q<Button>("passcodeButton")?.RegisterCallback<ClickEvent>(_ => ShowScreen("1122"));
+                break;
+
+            case "1121":
+            case "1122":
+                root.Q<Button>("CloseButton")?.RegisterCallback<ClickEvent>(_ => ShowScreen("1110"));
+                root.Q<Button>("CloseButtonBoth")?.RegisterCallback<ClickEvent>(_ => ShowScreen("1100"));
                 break;
         }
     }
